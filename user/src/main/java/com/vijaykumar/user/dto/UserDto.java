96809074f04298e9fe27babdc19891f5a7f7d309
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2022-01-24 12:48:31 +0530

    UserDto added

---
+++ user/src/main/java/com/vijaykumar/user/dto/UserDto.java
@@ -0,0 +1,49 @@
+package com.vijaykumar.user.dto;
+
+public class UserDto {
+    String firstName;
+    String lastName;
+    String email;
+
+    public UserDto() {
+    }
+
+    public UserDto(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

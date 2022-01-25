1aa5f4200e3d78a855c3f80e5c45e3f710d8485a
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2022-01-25 12:55:14 +0530

    chore : checking

---
+++ service-registry/src/test/java/com/vijaykumar/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaykumar.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
